75cdbfbd97d65ce35a32446371b6cec2c7991ea4
ch3n0l1 <ch3n0l1@gmail.com>
2018-05-07 01:20:19 +1000

    Add Magnet
    1. add magnet prefabs
    2. add ~ scripts:
      - moving via lerp

---
+++ Assets/Scripts/Magnet/MagnetAction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetAction : MonoBehaviour {
+
+    // Script for managing magnet movement
+
+    // Public Variables
+    public Vector2 newPosition;  // The target position
+    public float moveSpeed;
+
+    // Private Variables
+    private Transform trans;
+    private float limitDistance = 0.05f;
+
+    private void Awake() {
+        trans = transform;
+    }
+
+    private void Update() {
+        trans.position = Vector3.Lerp(trans.position, newPosition, Time.deltaTime * moveSpeed);
+
+        // Fix pos when extremly close
+        if (Mathf.Abs(newPosition.x - trans.position.x) < limitDistance) {
+            trans.position = newPosition;
+        }
+    }
+}
