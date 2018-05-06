3c5570553479815f3fda14a3137771b40d9c6eac
ch3n0l1 <ch3n0l1@gmail.com>
2018-05-07 03:16:18 +1000

    Now metal can move around magnet
    > Add metal movement function
    1. add sub-object to metal for detecting magnet
    2. add destory metal when crash to magnet

--- Assets/Scripts/Magnet/MetalAction.cs
+++ Assets/Scripts/Magnet/MetalAction.cs
@@ -4,13 +4,51 @@ using UnityEngine;
 
 public class MetalAction : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Script for managing metal movement
+
+    // Public variables
+    public float magnetStrength = 5.0f;
+    public float distanceStretch = 10.0f;  // strength, based on distance
+    public int magnetDirection = 1;  // 1 = attact, -1 = repel
+    public bool looseMagnet = true;
+
+    // Private variables
+    private Transform trans;
+    private Rigidbody2D rd;
+    private Transform magnetTrans;
+    private bool magnetInZone;
+
+    private void Awake() {
+        trans = transform;
+        rd = trans.GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate() {
+
+        if (magnetInZone) {
+            Vector2 directionToMagnet = magnetTrans.position - trans.position;  // control attact/repel
+            float distance = Vector2.Distance(magnetTrans.position, trans.position);  // control strength
+            float magnetDistanceStr = (distanceStretch / distance) * magnetStrength;
+
+            rd.AddForce(magnetDistanceStr * (directionToMagnet * magnetDirection));
+
+        }
+
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Magnet") {
+            magnetTrans = collision.transform;
+            magnetInZone = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.tag == "Magnet" && looseMagnet) {
+            magnetInZone = false;
+        }
+    }
+
 }
