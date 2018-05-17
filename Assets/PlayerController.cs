852e401dbf75dbc1e8a7bec8a18bdf387f1c9d1a
TrissyWhyWhy <37373447+TrissyWhyWhy@users.noreply.github.com>
2018-05-17 13:17:15 +1000

    added player

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 10.0f;
+        var y = Input.GetAxis("Vertical") * Time.deltaTime * 10.0f;
+
+        
+        transform.Translate(x, y, 0);
+    }
+}
