301efbc2222c5d513c6ad0c98345595e46b9023c
ch3n0l1 <ch3n0l1@gmail.com>
2018-05-21 16:21:55 +1000

    Add player movement for testing

---
+++ Assets/Scripts/Orbit/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f) * speed * Time.deltaTime;
+	}
+}
