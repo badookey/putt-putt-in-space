6fdc45d2409457c85bf38ea866632bcf9db10d26
ch3n0l1 <ch3n0l1@gmail.com>
2018-05-22 02:42:46 +1000

    Add 3 kinds player movement
    1. PlayerMovement: general movement via wasd;
    2. Swipe: swipe the screen to move object unit by unit;
    3. Spring: tap and drag to accumulate power, then drop to release.

---
+++ Assets/Scripts/Player/BowScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowScript : MonoBehaviour {
+
+    [Range(0, 1)]
+    public float power = 0;
+
+    Rigidbody2D rb;
+    
+    Vector2 startPoint;
+    Vector2 endPoint;
+
+    [SerializeField]
+    bool isDragging = false;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnMouseDown() {
+        startPoint = Camera.main.WorldToScreenPoint(Input.mousePosition);
+    }
+
+    void OnMouseUp() {
+
+        if (!isDragging) {
+            return;
+        }
+
+        endPoint = Camera.main.WorldToScreenPoint(Input.mousePosition);
+        Vector2 difference = startPoint - endPoint;
+        Debug.Log(difference.normalized);
+        rb.AddForce(difference * power);
+
+        isDragging = false;
+
+    }
+
+    void OnMouseDrag() {
+        
+        isDragging = true;
+    }
+
+}
---
+++ Assets/Scripts/Player/Swipe.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swipe : MonoBehaviour {
+
+
+    public float deadzoneRadius = 125f;
+
+    bool tap, swipeLeft, swipeRight, swipeUp, swipeDown;
+    bool isDragging = false;
+    Vector2 startTouch, swipeDelta;
+
+    void Update() {
+        tap = swipeLeft = swipeRight = swipeUp = swipeDown = false;
+
+        if (Input.GetMouseButtonDown(0)) {
+            tap = true;
+            isDragging = true;
+            startTouch = Input.mousePosition;
+        } else if (Input.GetMouseButtonUp(0)) {
+            isDragging = false;
+            Reset();
+        }
+
+        // Calculate the distance
+        swipeDelta = Vector2.zero;
+        if (isDragging) {
+            if (Input.GetMouseButton(0)) {
+                swipeDelta = (Vector2)Input.mousePosition - startTouch;
+            }
+        }
+
+        // Cross deadzone
+        if (swipeDelta.magnitude > deadzoneRadius) {
+            // Direction
+            float x = swipeDelta.x;
+            float y = swipeDelta.y;
+
+            if (Mathf.Abs(x) > Mathf.Abs(y)) {
+                // Left/right
+                if (x < 0) {
+                    swipeLeft = true;
+                } else {
+                    swipeRight = true;
+                }
+            } else {
+                // Up/down
+                if (y < 0) {
+                    swipeDown = true;
+                } else {
+                    swipeUp = true;
+                }
+            }
+
+            Reset();
+        }
+    }
+
+    void Reset() {
+        startTouch = swipeDelta = Vector2.zero;
+        isDragging = false;
+    }
+
+    public bool Tap { get { return tap; } }
+    public bool SwipeLeft { get { return swipeLeft; } }
+    public bool SwipeRight { get { return swipeRight; } }
+    public bool SwipeUp { get { return swipeUp; } }
+    public bool SwipeDown { get { return swipeDown; } }
+    public Vector2 SwipeDelta { get { return swipeDelta; } }
+}
