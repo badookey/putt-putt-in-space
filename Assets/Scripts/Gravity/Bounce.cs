761c0365f748908136905d4b94b65db4ce55d443
Tristan Hopper <11725645@student.uts.edu.au>
2018-05-22 15:59:15 +1000

    bounce

---
+++ Assets/Scripts/Gravity/Bounce.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Bounce : MonoBehaviour {
+    public Rigidbody2D rigidBody;
+    // Use this for initialization
+    void Start () {
+		
+	}
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Vector3 pos = col.gameObject.transform.position;
+        if(Math.Abs(pos.y)>10)
+        {
+            Vector2 vector = rigidBody.velocity;
+            rigidBody.velocity = new Vector2(vector.x, -vector.y);
+        }
+        if (Math.Abs(pos.x) > 17)
+        {
+            Vector2 vector = rigidBody.velocity;
+            rigidBody.velocity = new Vector2(-vector.x, vector.y);
+        }
+    }
+        // Update is called once per frame
+        void Update () {
+		
+	}
+}
