f19da6acea1dd08eb35594132865e70dfd46db50
ch3n0l1 <ch3n0l1@gmail.com>
2018-05-29 16:10:22 +1000

    Function Update
    1. new orbiting
    2. new accelerate control

---
+++ Assets/Scripts/Orbit/OrbitMotionManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitMotionManager : MonoBehaviour {
+
+    [Range(0, 360)]
+    public int orbitSpeed = 36;
+    public float orbitRadius = 5;
+    public List<OrbitRingObjectMotion> orbitRing;
+
+    private CircleCollider2D cc;
+    
+    private void Start() {
+        cc = GetComponent<CircleCollider2D>();
+        cc.radius = orbitRadius;
+
+        if (orbitRing == null)
+            orbitRing = new List<OrbitRingObjectMotion>();
+        else {
+            foreach (OrbitRingObjectMotion orom in orbitRing) {
+                
+
+            }
+        }
+    }
+
+
+    private void Update() {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            OrbitMotion otherOm = other.GetComponent<OrbitMotion>();
+            
+            // do something
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.tag == "Player") {
+            OrbitMotion otherOm = other.GetComponent<OrbitMotion>();
+
+            // do something
+        }
+    }
+
+    public Vector2 Center {
+        get { return transform.position; }
+    }
+}
---
+++ Assets/Scripts/Player/AccumulationMovement1.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccumulationMovement1 : MonoBehaviour {
+
+    public float power;
+    public float threshold;
+    public DragMode drageMode;
+
+    private Vector2 startPos;
+
+    private Rigidbody2D rb;
+    private OrbitMotion om;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        om = GetComponent<OrbitMotion>();
+    }
+
+
+
+    private void Update() {
+
+        if (Input.GetMouseButtonDown(0)) {  // start
+            startPos = Input.mousePosition;
+        } else if (Input.GetMouseButton(0)) {  // holding
+            //Debug.Log("accumulation: " + ((Vector2)Input.mousePosition - startPos).magnitude);
+        } else if (Input.GetMouseButtonUp(0)) {  // finish
+            Vector2 endPos = transform.position;
+            Vector2 newForce = (Vector2)Input.mousePosition - startPos;
+
+            
+            if (newForce.magnitude < threshold) {  // acceptable force
+
+                if (om.Active) {
+                    // *********************
+                    // release from orbiting
+                    // *********************
+                    Debug.Log("accumulation: " + ((Vector2)Input.mousePosition - startPos).magnitude);
+                    switch (drageMode) {
+                        case DragMode.forward:
+                            rb.AddForce(newForce * power);
+                            break;
+                        case DragMode.backward:
+                            rb.AddForce(-1.0f * newForce * power);
+                            break;
+                    }
+
+                } else {
+                    switch (drageMode) {
+                        case DragMode.forward:
+                            rb.AddForce(newForce * power);
+                            break;
+                        case DragMode.backward:
+                            rb.AddForce(-1.0f * newForce * power);
+                            break;
+                    }
+                }
+            }
+        }
+        
+    }
+
+}
+
+public enum DragMode {
+    forward, backward
+}
