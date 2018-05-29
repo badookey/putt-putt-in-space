2ff4eef70403726752d876bcec0dbc549178a8f3
Luke C <badookey@Hotmail.com>
2018-05-29 21:12:33 +1000

    Created Toolkit
    Updated reset script to ensure collision is between player and another object.
    
    Added heaps of prefabs to 'toolkit' for easy level creation.
    
    Updated Demo v2.0, Luke1, large orbit range demo.

---
+++ Assets/Scripts/Gameplay/reset.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class reset : MonoBehaviour {
+
+    public Text gameOverText;
+    // Use this for initialization
+    void Start () {
+    }
+    public IEnumerator ResetLevel()
+    {
+       
+        gameOverText.gameObject.SetActive(true);
+        Time.timeScale = 0.00001f;
+        yield return new WaitForSecondsRealtime(2);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StartCoroutine(ResetLevel());
+        }
+        
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
