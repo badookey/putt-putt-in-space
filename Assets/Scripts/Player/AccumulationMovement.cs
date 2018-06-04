0443b91ca8b2f549e787f95481ce5e9b4374e614
DamnBrain <39178389+DamnBrain@users.noreply.github.com>
2018-06-05 09:48:28 +1000

    Add files via upload

--- Assets/Scripts/Player/AccumulationMovement.cs
+++ Assets/Scripts/Player/AccumulationMovement.cs
@@ -8,11 +8,17 @@ public class AccumulationMovement : MonoBehaviour {
     public float threshold = 500f;
     public DragMode drageMode = DragMode.backward;
 
-    private Vector2 startPos;
+    private Vector3 startPos;
+    private Vector3 _sizeStart;
+    private Vector3 _scaleStart;
+    public float speed = 5f;
+    public Transform target;
 
     private Rigidbody2D rb;
     private OrbitMotion om;
     private int _validHits;
+
+
 
     private void Start() {
         rb = GetComponent<Rigidbody2D>();
@@ -25,17 +31,36 @@ public class AccumulationMovement : MonoBehaviour {
     
     private void Update() {
 
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
+
         if (Input.GetKeyDown("y"))
         {
             SwapDragMode();
         }
-        if (Input.GetMouseButtonDown(0)) {  // start
+        if (Input.GetMouseButtonDown(0))
+        {  // start
             startPos = Input.mousePosition;
-        } else if (Input.GetMouseButton(0)) {  // holding
-            //Debug.Log("accumulation: " + ((Vector2)Input.mousePosition - startPos));
-        } else if (Input.GetMouseButtonUp(0)) {  // finish
+            
+        }
+        else if (Input.GetMouseButton(0))
+        {  // holding
+           //Debug.Log("accumulation: " + ((Vector2)Input.mousePosition - startPos));
+           //var _pointerTravel = (Vector2)(Input.mousePosition) - startPos;
+           //var _rotation = ((_sizeStart.y + _pointerTravel.y) / _sizeStart.y) * _scaleStart.y;
+           //transform.rotation = new Vector3(0, 0, _rotation);
+           //Get mouse position
+            //var _pointerTravel = (Vector3)(Input.mousePosition) - startPos;
+            //var _scaleY = ((_sizeStart.y + _pointerTravel.y) / _sizeStart.y) * _scaleStart.y;
+            //transform.localScale = new Vector3(transform.localScale.x, _scaleY, transform.localScale.z);
+
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {  // finish
             Vector2 endPos = transform.position;
-            Vector2 newForce = (Vector2)Input.mousePosition - startPos;
+            Vector2 newForce = (Vector3)Input.mousePosition - startPos;
 
             // plan 1
             //if (newForce.magnitude < threshold) {  // acceptable force
@@ -50,15 +75,17 @@ public class AccumulationMovement : MonoBehaviour {
             //            break;
             //    }
             //}
-            
+
             // plan 2
-            if (newForce.magnitude > threshold) {  // acceptable force
+            if (newForce.magnitude > threshold)
+            {  // acceptable force
                 newForce = Vector3.ClampMagnitude(newForce, threshold);
             }
 
             _validHits += 1;  // count valid hit 
 
-            switch (drageMode) {
+            switch (drageMode)
+            {
                 case DragMode.forward:
                     rb.AddForce(newForce * power);
                     break;
@@ -93,3 +120,4 @@ public class AccumulationMovement : MonoBehaviour {
 public enum DragMode {
     forward, backward
 }
+
