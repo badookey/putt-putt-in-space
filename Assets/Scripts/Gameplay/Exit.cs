377d9802ad363807b076ec04e48fc727e83921e6
Tristan Hopper <11725645@student.uts.edu.au>
2018-06-04 20:03:29 +1000

    FIxed menu

--- Assets/Scripts/Gameplay/Exit.cs
+++ Assets/Scripts/Gameplay/Exit.cs
@@ -10,10 +10,50 @@ public class Exit : MonoBehaviour {
 	void Start () {
 		
 	}
+    private Rect windowRect = new Rect((Screen.width - 200) / 2, (Screen.height - 300) / 2, 200, 300);
+    // Only show it if needed.
+    private bool show = false;
+
+    void OnGUI()
+    {
+        if (show)
+            windowRect = GUI.Window(0, windowRect, DialogWindow, "You Won");
+    }
+
+    // This is the actual window.
+    void DialogWindow(int windowID)
+    {
+        Time.timeScale = 0;
+        float y = 20;
+        float[] a = HighScore.Save(SceneManager.GetActiveScene().name);
+        GUI.Label(new Rect(5, y, windowRect.width, 20), string.Format("Your score: {0} Time: {1}", (int)a[0], a[1]));
+        GUI.Label(new Rect(5, 2*y, windowRect.width, 20), string.Format("High score: {0} Time: {1}", (int)a[2], a[3]));
+
+        if (GUI.Button(new Rect(5, 3*y, windowRect.width - 10, 20), "Next Level"))
+        {
+            SceneManager.LoadScene(nextscenename);
+            Time.timeScale = 1;
+            show = false;
+        }
+        if (GUI.Button(new Rect(5, 4*y, windowRect.width - 10, 20), "Main Menu"))
+        {
+            SceneManager.LoadScene("Menu");
+            Time.timeScale = 1;
+            show = false;
+        }
+        if (GUI.Button(new Rect(5, 5*y, windowRect.width - 10, 20), "Exit"))
+        {
+            Application.Quit();
+            Time.timeScale = 1;
+            show = false;
+        }
+    }
     public void GameOver()
     {
+        show = true;
         //GameObject gameObject = GameObject.Find("Victory");
         //gameObject.SetActive(true);
+        /*
         float[] a = HighScore.Save(SceneManager.GetActiveScene().name);
         int option = EditorUtility.DisplayDialogComplex("You won!",string.Format("Your score: {0} Time: {1} \n High score: {2} Time: {3}",(int)a[0],a[1],(int)a[2],a[3]),
                 "Next Level",
@@ -40,9 +80,10 @@ public class Exit : MonoBehaviour {
             default:
                 Debug.LogError("Unrecognized option.");
                 break;
-        }
-        
+                */
     }
+        
+    
     // Update is called once per frame
     void Update () {
 		
