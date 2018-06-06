5110faf1a2ddde42c85629af6bbc4cd9bc521a47
Tristan Hopper <11725645@student.uts.edu.au>
2018-06-06 16:24:16 +1000

    fixed bug in pause

--- Assets/Scripts/Player/AccumulationMovement.cs
+++ Assets/Scripts/Player/AccumulationMovement.cs
@@ -30,68 +30,70 @@ public class AccumulationMovement : MonoBehaviour {
     }
     
     private void Update() {
-
-        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
-
-        if (Input.GetKeyDown("y"))
+        if (Time.timeScale > 0)
         {
-            SwapDragMode();
-        }
-        if (Input.GetMouseButtonDown(0))
-        {  // start
-            startPos = Input.mousePosition;
-            
-        }
-        else if (Input.GetMouseButton(0))
-        {  // holding
-           //Debug.Log("accumulation: " + ((Vector2)Input.mousePosition - startPos));
-           //var _pointerTravel = (Vector2)(Input.mousePosition) - startPos;
-           //var _rotation = ((_sizeStart.y + _pointerTravel.y) / _sizeStart.y) * _scaleStart.y;
-           //transform.rotation = new Vector3(0, 0, _rotation);
-           //Get mouse position
-            //var _pointerTravel = (Vector3)(Input.mousePosition) - startPos;
-            //var _scaleY = ((_sizeStart.y + _pointerTravel.y) / _sizeStart.y) * _scaleStart.y;
-            //transform.localScale = new Vector3(transform.localScale.x, _scaleY, transform.localScale.z);
+            Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
 
-        }
-        else if (Input.GetMouseButtonUp(0))
-        {  // finish
-            Vector2 endPos = transform.position;
-            Vector2 newForce = (Vector3)Input.mousePosition - startPos;
-
-            // plan 1
-            //if (newForce.magnitude < threshold) {  // acceptable force
-            //    _validHits += 1;  // count valid hit 
-
-            //    switch (drageMode) {
-            //        case DragMode.forward:
-            //            rb.AddForce(newForce * power);
-            //            break;
-            //        case DragMode.backward:
-            //            rb.AddForce(-1.0f * newForce * power);
-            //            break;
-            //    }
-            //}
-
-            // plan 2
-            if (newForce.magnitude > threshold)
-            {  // acceptable force
-                newForce = Vector3.ClampMagnitude(newForce, threshold);
-            }
-
-            _validHits += 1;  // count valid hit 
-
-            switch (drageMode)
+            if (Input.GetKeyDown("y"))
             {
-                case DragMode.forward:
-                    rb.AddForce(newForce * power);
-                    break;
-                case DragMode.backward:
-                    rb.AddForce(-1.0f * newForce * power);
-                    break;
+                SwapDragMode();
+            }
+            if (Input.GetMouseButtonDown(0))
+            {  // start
+                startPos = Input.mousePosition;
+
+            }
+            else if (Input.GetMouseButton(0))
+            {  // holding
+               //Debug.Log("accumulation: " + ((Vector2)Input.mousePosition - startPos));
+               //var _pointerTravel = (Vector2)(Input.mousePosition) - startPos;
+               //var _rotation = ((_sizeStart.y + _pointerTravel.y) / _sizeStart.y) * _scaleStart.y;
+               //transform.rotation = new Vector3(0, 0, _rotation);
+               //Get mouse position
+               //var _pointerTravel = (Vector3)(Input.mousePosition) - startPos;
+               //var _scaleY = ((_sizeStart.y + _pointerTravel.y) / _sizeStart.y) * _scaleStart.y;
+               //transform.localScale = new Vector3(transform.localScale.x, _scaleY, transform.localScale.z);
+
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {  // finish
+                Vector2 endPos = transform.position;
+                Vector2 newForce = (Vector3)Input.mousePosition - startPos;
+
+                // plan 1
+                //if (newForce.magnitude < threshold) {  // acceptable force
+                //    _validHits += 1;  // count valid hit 
+
+                //    switch (drageMode) {
+                //        case DragMode.forward:
+                //            rb.AddForce(newForce * power);
+                //            break;
+                //        case DragMode.backward:
+                //            rb.AddForce(-1.0f * newForce * power);
+                //            break;
+                //    }
+                //}
+
+                // plan 2
+                if (newForce.magnitude > threshold)
+                {  // acceptable force
+                    newForce = Vector3.ClampMagnitude(newForce, threshold);
+                }
+
+                _validHits += 1;  // count valid hit 
+
+                switch (drageMode)
+                {
+                    case DragMode.forward:
+                        rb.AddForce(newForce * power);
+                        break;
+                    case DragMode.backward:
+                        rb.AddForce(-1.0f * newForce * power);
+                        break;
+                }
             }
         }
     }
