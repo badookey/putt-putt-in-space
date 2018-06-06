3d58abcf0c7ab9eaf5199f0d591a6347f3dccbb9
Tristan Hopper <11725645@student.uts.edu.au>
2018-06-06 21:20:29 +1000

    music

---
+++ Assets/singleton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class singleton : MonoBehaviour {
+
+    private static singleton instance = null;
+    public static singleton Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this) {
+            Destroy(this.gameObject);
+            return;
+        } else {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
